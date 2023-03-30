1eb54bad553a4fd21fb7ca03edae4c3c68bfe605
GangGreenTemperTatum <104169244+GangGreenTemperTatum@users.noreply.github.com>
2023-03-29 20:04:27 -0700

    Create basic_read_input_to_stdout.sh

---
+++ basic_read_input_to_stdout.sh
@@ -0,0 +1,8 @@
+#!/usr/local/bin/bash
+echo "What is your name?"
+read name
+echo "What is your password?"
+read -s pass
+read -p "What's your favorite animal? " animal
+
+echo "name: $name, pass: $pass, animal: $animal"
