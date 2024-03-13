c2d7a66d9c3330229236751d805976d493031574
Ads Dawson <104169244+GangGreenTemperTatum@users.noreply.github.com>
2024-03-12 21:37:36 -0700

    Update monitor-new-domains-with-subfinder-anew.sh

--- monitor-new-domains-with-subfinder-anew.sh
+++ monitor-new-domains-with-subfinder-anew.sh
@@ -1,7 +1,9 @@
 while true;
   # For cohere.ai
-  subfinder -silent -dL target1.txt -all | anew target1subs.txt | notify
+  subfinder -silent -dL target1.txt -all | anew target1subs.txt | notify -bulk -pc ./providers.yaml
   # For cohere.com
-  subfinder -silent -dL target2.txt -all | anew target2subs.txt | notify
+  subfinder -silent -dL target2.txt -all | anew target2subs.txt | notify -bulk -pc ./providers.yaml
  sleep 3600;
 done
+
+# run script as a systemd service file
