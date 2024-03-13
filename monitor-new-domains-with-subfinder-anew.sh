1791d5f6b4744dd54c5d8c06f8a0f080216cbb82
Ads Dawson <104169244+GangGreenTemperTatum@users.noreply.github.com>
2024-03-12 21:13:51 -0700

    Update monitor-new-domains-with-subfinder-anew.sh

--- monitor-new-domains-with-subfinder-anew.sh
+++ monitor-new-domains-with-subfinder-anew.sh
@@ -1,4 +1,7 @@
 while true;
-do subfinder -silent -dL $DOMAINSFILE -all | anew subdomains.txt | notify
-  sleep 3600;
+  # For cohere.ai
+  subfinder -silent -dL target1.txt -all | anew target1subs.txt | notify
+  # For cohere.com
+  subfinder -silent -dL target2.txt -all | anew target2subs.txt | notify
+ sleep 3600;
 done
